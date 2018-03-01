336d9433e93a4a32b2a8fb9d03a798cbe476eca7
dartse <33062156+dartse@users.noreply.github.com>
2018-03-01 18:01:52 +0000

    No idea
    Some stuff changed

--- Club-Programming Lectures/src/ClubManager.java
+++ Club-Programming Lectures/src/ClubManager.java
@@ -3,17 +3,44 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 public class ClubManager {
+	public static final int NAME_INDEX = 0;
+	public static final int ADDRESS_INDEX = 1;
+	public static final int YEAR_OF_REG_INDEX = 2;
+	public static final int EMAIL_INDEX = 3;
+	public static int groupId = 101;
 	
 	public static void main(String[] args) {
 		ArrayList<ClubMember> clubMemberList = new ArrayList<ClubMember>();
-		readMembersFromFile(clubMemberList);
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		boolean finished = false;
+		ClubManager manager = new ClubManager();
+		while(!finished) {
+			System.out.println("Please select 1 to create a group, 2 to view club members, 3 to view details of a member.........");
+			
+			//create a group and provide group description
+			String groupDesc = "Test";
+			groupList.add(manager.CreateGroup(groupId++, groupDesc));
+			
+			//manager wants to see club members
+			manager.printMembers(clubMemberList);
+			
+			//manager wants to change user address and/email
+			String memberName = "Adam Adamson";
+			String address = "Another lecture hall";
+			String email = null;
+			manager.updateMember(manager.getMember(clubMemberList, memberName), address, email);
+		}
 	}
 
-	public void printMembers(ArrayList<ClubMember> clubMembersList) {
-
+	public void printMembers(ArrayList<ClubMember> clubMemberList) {
+		String clubMemberNameString = "The names of the club members are: ";
+		for (ClubMember clubMember : clubMemberList) {
+			clubMemberNameString += clubMember.getName() + ", ";
+		}
+		System.out.println(clubMemberNameString.substring(0, clubMemberNameString.lastIndexOf(',')));
 	}
 
-	public static void readMembersFromFile(ArrayList<ClubMember> clubMemberList) {
+	public void readMembersFromFile(ArrayList<ClubMember> clubMemberList) {
 		FileReader reader;
 		try {
 			reader = new FileReader("Members.txt");
@@ -25,7 +52,8 @@ public class ClubManager {
 					endFound = true;
 				} else {
 					String[] memberInfo = memberData.split(",");
-					clubMemberList.add(new ClubMember(memberInfo[0], memberInfo[1], Integer.parseInt(memberInfo[2]), memberInfo[3]));
+					clubMemberList.add(new ClubMember(memberInfo[NAME_INDEX], memberInfo[ADDRESS_INDEX], Integer.parseInt(memberInfo[YEAR_OF_REG_INDEX]),
+							memberInfo[EMAIL_INDEX]));
 				}
 			}
 			buffReader.close();
@@ -34,4 +62,46 @@ public class ClubManager {
 		}
 	}
 
+	public ClubMember getMember(ArrayList<ClubMember> clubMemberList, String name) {
+		for (ClubMember clubMember : clubMemberList) {
+			if (clubMember.getName().equals(name))
+				return clubMember;
+		}
+		return null;
+	}
+
+	public void updateMember(ClubMember clubMember, String address, String email) {
+		if (clubMember != null) {
+			if (email != null)
+				clubMember.setName(email);
+			if (address != null)
+				clubMember.setAddress(address);
+		}
+	}
+
+	public Group CreateGroup(int groupId, String groupDescription) {
+		return new Group(groupId, groupDescription);
+	}
+
+	public boolean addMemberToGroup(Group group, ClubMember member) {
+		if(group != null && member != null) {
+			return group.addGroupMember(member);
+		}
+		return false;
+	}
+	
+	public void printGroupMembers(Group group) {
+		if(group != null) {
+			System.out.println("The members of the group" + group.getGroupDescription() + " are:");
+			for(ClubMember clubMember : group.getGroupMembers()) {
+				System.out.println(clubMember.getName());
+			}
+		}
+	}
+	
+	public void printGroupNamesAndNumberOfMembers(ArrayList<Group> groupList) {
+		for(Group group : groupList) {
+			System.out.println("There are " + group.getNumberOfMembers() + "members in the group: " + group.getGroupDescription());
+		}
+	}
 }

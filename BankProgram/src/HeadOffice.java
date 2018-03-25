2dc903616a45df31a758a66bc0167cf79f5a64b6
dartse <33062156+dartse@users.noreply.github.com>
2018-03-26 00:41:28 +0100

    I don't know
    I think this is mostly just lecture stuff.

--- BankProgram/src/HeadOffice.java
+++ BankProgram/src/HeadOffice.java
@@ -1,16 +1,45 @@
+import java.io.*;
 
 abstract public class HeadOffice {
 	String bankName;
 	String address;
-	
+
 	private long accountNumber = 10000000;
-	private int sortCode  = 991100;
-	
+	private int sortCode = 991100;
+
 	public long createAccountNumber() {
-		return accountNumber++;
+		try {
+			BufferedReader buffReader = new BufferedReader(new FileReader("accountNumber.txt"));
+			boolean endOfFileFound = false;
+			while (!endOfFileFound) {
+				String aNumString = buffReader.readLine();
+				if (aNumString == null) {
+					endOfFileFound = true;
+				} else {
+					accountNumber = Long.parseLong(aNumString);
+				}
+			}
+		} catch (Exception e) {
+
+		}
+		if(accountNumber > 0)
+			return ++accountNumber;
+		return accountNumber;
 	}
-	
+
 	public int createSortCode() {
 		return sortCode++;
+	}
+
+	public HeadOffice() {
+		try {
+			BufferedWriter buffWriter = new BufferedWriter(new FileWriter("accountNumber.txt"));
+			buffWriter.write(String.valueOf(this.accountNumber));
+			buffWriter = new BufferedWriter(new FileWriter("sortCode.txt"));
+			buffWriter.write(String.valueOf(this.sortCode));
+			buffWriter.close();
+		} catch (Exception e) {
+
+		}
 	}
 }
